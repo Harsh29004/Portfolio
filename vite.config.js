dab2854cb4c9b6121785b21295055a06f5045351
Harsh Panchal <harshpanchal2904@gmail.com>
2025-11-08 18:50:35 +0530

    conflict

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'framer-motion': ['framer-motion'],
+          'icons': ['react-icons']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
